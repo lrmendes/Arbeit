086e6efcda9abd2bee1d7b7d44ebf797f6c8a955
Lucas Mendes <lrmen14@gmail.com>
2020-06-10 17:33:29 +0100

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -8,15 +8,16 @@ import "firebase/firestore"
 //import "firebase/functions";
 //import "firebase/storage";
 
-// Initialize Firebase
+// Initialize Firebase - 
+// PUT Your FirebaseAPP Config Here
 const firebaseConfig = {
-    apiKey: "AIzaSyCw2UrczMRS3EKBxjJNvHxa4TmNUE0UbiA",
-    authDomain: "arbeit-d110e.firebaseapp.com",
-    databaseURL: "https://arbeit-d110e.firebaseio.com",
-    projectId: "arbeit-d110e",
-    storageBucket: "arbeit-d110e.appspot.com",
-    messagingSenderId: "1030326602485",
-    appId: "1:1030326602485:web:727dab5be865892c5d13bc"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 firebase.initializeApp(firebaseConfig);
